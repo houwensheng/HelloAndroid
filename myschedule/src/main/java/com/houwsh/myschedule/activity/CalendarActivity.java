57ad96e66f3795fff4ef8c02c7e2466d3310de78
houwensheng <houwsh@outlook.com>
2019-01-05 22:08:28 +0800

    before workout

---
+++ myschedule/src/main/java/com/houwsh/myschedule/activity/CalendarActivity.java
@@ -0,0 +1,15 @@
+package com.houwsh.myschedule.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.houwsh.myschedule.R;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
